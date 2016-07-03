0b18bf4488c7ee71312603323820e16e2e56a937
qlquanle <lechinhquan@gmail.com>
2016-07-02 20:41:31 -0400

    Add preamble and top level links to identify.py

---
+++ identify.py
@@ -0,0 +1,17 @@
+#identify.py
+
+#Purpose: Identify poems on w#www.internal.org written by Robert Frost and collect their links.
+
+#Input
+
+#Output
+
+#www.internal.org puts all RF poems on the following link, separated in five different pages
+
+toplink = 'http://www.internal.org/robert_frost'
+
+#For easy sorting we add the modifiers that sort these poems in alphabetical order.
+
+sorting = '?a=a&search=&l=1&y=0'
+
+

25af1e918d84fb11bf5960062055d44560dba061
qlquanle <lechinhquan@gmail.com>
2016-07-03 13:53:08 -0400

    Add code to create list of links to RF poems.

--- identify.py
+++ identify.py
@@ -6,7 +6,22 @@
 
 #Output
 
-#www.internal.org puts all RF poems on the following link
+#Imports
+import requests
+from lxml import html
+import re
 
+#www.internal.org puts all RF poems on the following link
 toplink = 'http://www.internal.org/robert_frost'
 
+#The following lines collect the html page.
+page = requests.get(toplink)
+tree = html.fromstring(page.content)
+
+#Collect links to poems
+link_list = []
+for link in tree.xpath('//a/@href'):
+    if re.search("\/Robert_Frost\/",link) != None:
+        link_list.append(link)
+
+print link_list
